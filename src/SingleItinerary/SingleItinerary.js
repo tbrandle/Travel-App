9c0c8bbe1b0426f842ece966466e41f0e4255cb7
Tim <tbrandle53@gmail.com>
2017-04-18 17:32:00 -0600

    single destination style

--- src/SingleItinerary/SingleItinerary.js
+++ src/SingleItinerary/SingleItinerary.js
@@ -3,21 +3,36 @@ import GoogleInitialMap from '../GoogleMap/GoogleInitialMap';
 import './SingleItinerary.css';
 
 
+const renderDestinations = (destinations) => {
+  return destinations.map((obj, i) => {
+    console.log(obj);
+    return (
+        <div className="single-destination" >
+          <p className="place">{obj.place}</p>
+          <p>{obj.placeDescription}</p>
+        </div>
+    )
+  })
+}
 
 const SingleItinerary = ({match, itineraries}) => {
-  console.log(destinations);
   const { id } = match.params
   const itinerary = itineraries.find(obj => obj.id === parseInt(id))
   const { destinations } = itinerary
    return (
     <div>
-      {itinerary.title}
-      {itinerary.body}
       <div className='single-map-container'>
         <GoogleInitialMap containerElement={ <div style={{ height: "100%"}}/> }
           mapElement={ <div style={{ height: "100%"}}/> }
           destinations={destinations}
           />
+      </div>
+      <div className="title-card">
+        <p className='itinerary-title'>{itinerary.title}</p>
+        <p className='itinerary-description'>{itinerary.description}</p>
+      </div>
+      <div className="render-destination-wrapper">
+        {renderDestinations(itinerary.destinations)}
       </div>
     </div>
   )
