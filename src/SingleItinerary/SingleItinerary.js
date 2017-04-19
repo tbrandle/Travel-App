f973fa72ae6c5c3af92e890d3a299c9ac4f0ec4b
Tim <tbrandle53@gmail.com>
2017-04-19 10:08:12 -0600

    add likes

--- src/SingleItinerary/SingleItinerary.js
+++ src/SingleItinerary/SingleItinerary.js
@@ -6,6 +6,23 @@ import './SingleItinerary.css';
 
 export default class SingleItinerary extends Component {
 
+  constructor(){
+    super()
+    this.state={
+      itinerary:{}
+    }
+  }
+
+  componentDidMount(){
+    const { match, itineraries } = this.props
+    const id = parseInt(match.params.id)
+    const itinerary = itineraries.find(obj => obj.id === id)
+    if (!itinerary.likes) {
+      itinerary['likes'] = 0
+    }
+    this.setState({ itinerary })
+  }
+
   renderDestinations(destinations){
     return destinations.map((obj, i) => {
       return (
@@ -27,47 +44,47 @@ export default class SingleItinerary extends Component {
     this.props.addToWishList(newUser)
     database.ref('users').update({[this.props.currentUser.uid]:newUser})
   }
-  addLike(itinerary){
+
+  addLike(){
     const { itineraries } = this.props
+    const { itinerary } = this.state;
     const index = itineraries.indexOf(itinerary)
-    if (!itinerary.likes) {
-      itinerary['likes'] = 0
-    }
-    itinerary.likes ++;
+    const newLikes = itinerary.likes ++
+    this.setState({likes:newLikes});
     itineraries.splice(index, 1, itinerary)
     this.props.updateItinerary(itineraries)
-
-
-    // if theres a like
-    // add one
-
-    //else create like object and add one
   }
 
   render(){
-    const { match, itineraries, addToWishList } = this.props
-    const id = parseInt(match.params.id)
-    const itinerary = itineraries.find(obj => obj.id === id)
-    const { destinations } = itinerary
-
-     return (
-      <div>
-        <div className='single-map-container'>
-          <GoogleInitialMap containerElement={ <div style={{ height: "100%"}}/> }
-            mapElement={ <div style={{ height: "100%"}}/> }
-            destinations={destinations}
-            />
+    // const { addToWishList } = this.props
+    const { itinerary, itinerary:{destinations}, itinerary:{id} } = this.state
+    // debugger
+    if (destinations) {
+       return (
+        <div>
+          <div className='single-map-container'>
+            <GoogleInitialMap containerElement={ <div style={{ height: "100%"}}/> }
+              mapElement={ <div style={{ height: "100%"}}/> }
+              destinations={destinations}
+              />
+          </div>
+          <div className="title-card">
+            <p className='itinerary-title'>{itinerary.title}</p>
+            <div className="likes-wrapper">
+              <img className="heart-img" src={(require('../../images/heart.svg'))} />
+              <p className="likes">+{itinerary.likes}</p>
+            </div>
+            <p className='itinerary-description'>{itinerary.description}</p>
+          </div>
+          <button className="btn" onClick={() => this.addLike(itinerary)}>like</button>
+          <button className="btn" onClick={() => this.handleClick(id) }>add to wish list</button>
+          <div className="render-destination-wrapper">
+            { this.renderDestinations(itinerary.destinations) }
+          </div>
         </div>
-        <div className="title-card">
-          <p className='itinerary-title'>{itinerary.title}</p>
-          <p className='itinerary-description'>{itinerary.description}</p>
-        </div>
-        <button onClick={() => this.addLike(itinerary)}>like</button>
-        <button className="btn" onClick={() => this.handleClick(id) }>add to wish list</button>
-        <div className="render-destination-wrapper">
-          { this.renderDestinations(itinerary.destinations) }
-        </div>
-      </div>
-    )
+      )
+    } else {
+      return <div></div>
+    }
   }
 }
